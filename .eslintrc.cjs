da9bbf71b20ace69da14e45d467d88062b62ad3c
JIMIN1020 <jimins4920@gmail.com>
2024-07-14 23:48:39 +0900

    [CHORE/HM-49] 불필요한 파일 제거, tsconfig 및 eslint config 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,14 @@ module.exports = {
   },
   plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
+    'import/no-extraneous-dependencies': 'warn',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'function-declaration', // 컴포넌트 선언 시 function 선언문 사용
+        unnamedComponents: 'arrow-function',
+      },
+    ],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
@@ -31,6 +39,31 @@ module.exports = {
         jsx: 'never',
         ts: 'never',
         tsx: 'never',
+      },
+    ],
+    '@typescript-eslint/naming-convention': [
+      // 네이밍 컨벤션
+      'warn',
+      {
+        selector: 'typeAlias', // 타입 선언
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'interface', // 인터페이스 선언
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE'],
+      },
+      {
+        selector: 'function', // exported function (컴포넌트 명)
+        format: ['PascalCase'],
+        modifiers: ['exported'],
+      },
+      {
+        selector: 'function', // function
+        format: ['camelCase'],
       },
     ],
   },
