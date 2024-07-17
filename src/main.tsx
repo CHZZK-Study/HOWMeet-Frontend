1bc5d1be867916194685e861cb27b5a1eb34844a
JIMIN1020 <jimins4920@gmail.com>
2024-07-17 20:57:06 +0900

    [STYLE/HM-50]: theme 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './styles/theme';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  color: {
+    white: '#FFFFFF',
+  },
+  fontSize: {
+    base: '14px',
+  },
+} as const;
+
+export default theme;
