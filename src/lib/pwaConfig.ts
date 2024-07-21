77433f5f5e60a0f568341979af9d5bb7d5e15d6c
JIMIN1020 <jimins4920@gmail.com>
2024-07-21 22:48:11 +0900

    [CHORE/HM-70]: PWA config 설정 (w. 임시 아이콘)

---
+++ src/lib/pwaConfig.ts
@@ -0,0 +1,38 @@
+import { type VitePWAOptions } from 'vite-plugin-pwa';
+
+export const pwaConfig: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+  manifest: {
+    name: 'HowMeet',
+    short_name: 'HowMeet',
+    description: '',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#FFFFFF',
+    theme_color: '#ffffff',
+    icons: [
+      {
+        src: 'assets/icons/favicon.ico',
+        sizes: '64x64',
+        type: 'image/x-icon',
+      },
+      {
+        src: 'assets/icons/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'assets/icons/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  },
+};
