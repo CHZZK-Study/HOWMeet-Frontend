7e3dcaf05f4286b231ce0bdd1181ae3f8e38ea08
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-02 03:06:42 +0900

    [STYLE/HM-71]: 날짜 선택 컴포넌트 UI 구현

---
+++ src/components/room/SelectDate.tsx
@@ -0,0 +1,46 @@
+import ICONS from '@/constants/icons';
+import { Badge } from '@/styles/components/badge';
+import {
+  SelectableItem,
+  SelectContainer,
+  SelectWrapper,
+} from '@/styles/components/meeting/select';
+import { SubTitle } from '@/styles/components/text';
+import styled from 'styled-components';
+
+function SelectDate() {
+  return (
+    <SelectContainer>
+      <SubTitle>날짜 선택</SubTitle>
+      <SelectWrapper>
+        <SelectableItem>
+          <DateWrapper>
+            <Badge>시작일</Badge>
+            <Date>2024. 07. 11</Date>
+          </DateWrapper>
+          <img src={ICONS.common.right} alt="right" />
+        </SelectableItem>
+        <SelectableItem>
+          <DateWrapper>
+            <Badge>시작일</Badge>
+            <Date>2024. 07. 11</Date>
+          </DateWrapper>
+          <img src={ICONS.common.right} alt="right" />
+        </SelectableItem>
+      </SelectWrapper>
+    </SelectContainer>
+  );
+}
+
+const DateWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const Date = styled.div`
+  color: ${({ theme }) => theme.color.secondary.solid.bk[700]};
+  ${({ theme }) => theme.typo.body.medium[14]};
+  letter-spacing: -0.14px;
+`;
+
+export default SelectDate;
