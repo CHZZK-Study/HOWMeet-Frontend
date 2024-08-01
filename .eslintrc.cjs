f61c92b89af9c0c7f505b68d5597c11238d9a198
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-02 00:32:54 +0900

    [FIX/HM-71]: eslint config 파일 경로 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,7 +14,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: ['./tsconfig.app.json', './tsconfig.node.json'],
+    project: ['./tsconfig.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
   },
   plugins: ['react-refresh', '@typescript-eslint', 'prettier'],
   rules: {
