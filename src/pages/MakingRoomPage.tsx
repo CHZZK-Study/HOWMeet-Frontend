1be49299e088a821a4e51fd57da07609b6f2c815
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-02 00:31:13 +0900

    [STYLE/HM-71]: 메인 페이지 UI 컴포넌트 구현

---
+++ src/pages/MakingRoomPage.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import {
+  ContentContainer,
+  FlexColContainer,
+} from '@/styles/components/container';
+import Button from '@/components/common/Button';
+import RoomInput from '@/components/room/RoomInput';
+import Header from '@/components/common/Header';
+import HEAD_TITLE from '@/constants/header';
+import INPUT from '@/constants/input';
+import TITLE from '@/constants/title';
+import { PageTitle } from '@/styles/components/text';
+
+import { useForm } from 'react-hook-form';
+
+function MakingRoomPage() {
+  const methods = useForm({ mode: 'onChange' });
+  const {
+    register,
+    formState: { isValid },
+  } = methods;
+
+  return (
+    <FlexColContainer>
+      <Header title={HEAD_TITLE.makeRoom} />
+      <ContentContainer>
+        <PageTitle>{TITLE.makeRoom}</PageTitle>
+        <RoomInput
+          placeholder={INPUT.makeRoom.placeholder}
+          label={INPUT.makeRoom.label}
+          {...register('roomName', {
+            required: true,
+            minLength: 1,
+          })}
+        />
+      </ContentContainer>
+      <ButtonContainer>
+        <Button disabled={!isValid}>다음</Button>
+      </ButtonContainer>
+    </FlexColContainer>
+  );
+}
+
+const ButtonContainer = styled.div`
+  position: sticky;
+  bottom: 16px;
+
+  width: 100%;
+
+  padding: 0 24px;
+`;
+
+export default MakingRoomPage;
