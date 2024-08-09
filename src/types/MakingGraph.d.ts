c056539c7696e5f3ef21d20092edb275f6e16033
oyeong011 <oyeong011@gmail.com>
2024-08-09 15:17:29 +0900

    [REFACTOR/HM-94] 그래프 계산 타입 리팩토링

---
+++ src/types/MakingGraph.d.ts
@@ -0,0 +1,12 @@
+export interface TimeSlot {
+  time: string;
+  users: string[];
+  userCount: number;
+}
+
+export interface RankedTimeSlot {
+  startTime: string;
+  endTime: string;
+  users: string[];
+  rank: number;
+}
