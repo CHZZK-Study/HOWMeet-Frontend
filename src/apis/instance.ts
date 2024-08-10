bbb60484139beb9117aeb321e6a88db2204a0bf0
JIMIN1020 <jimins4920@gmail.com>
2024-08-10 13:54:54 +0900

    [API/HM-88]: axios intance 세팅

---
+++ src/apis/instance.ts
@@ -0,0 +1,23 @@
+import { BASE_URL, DEFAULT_TIMOUT } from '@/constants/api';
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: DEFAULT_TIMOUT,
+});
+
+axiosInstance.interceptors.request.use(
+  (req) => {
+    return req;
+  },
+  (err) => Promise.reject(err)
+);
+
+axiosInstance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (err) => {
+    return Promise.reject(err);
+  }
+);
---
+++ src/constants/api.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_TIMOUT = 30000;
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
