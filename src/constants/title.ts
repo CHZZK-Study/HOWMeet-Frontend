9e1c57e783a422227ea75dd285383e7be3dbe07b
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-16 03:57:42 +0900

    [REFACTOR/HM-90]: 페이지 제목 상수화

--- src/constants/title.ts
+++ src/constants/title.ts
@@ -2,6 +2,7 @@ export const TITLE = {
   makeRoom: '새로운 방의 이름을 정해주세요',
   newMeeting: '첫 일정도 만들어 보세요',
   confirmMeeting: '일정 정보를 확인해 주세요',
+  attendRoom: '참여 중인 방',
 };
 
 export const SUB_TITLE = {
