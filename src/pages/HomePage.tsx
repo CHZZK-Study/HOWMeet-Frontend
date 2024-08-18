f5ef5715f1352883c1bc10f8008966ae0d4eccae
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-18 21:40:44 +0900

    [STYLE/HM-102]: 홈화면 컴포넌트 구현

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,63 @@
+import HomeHeader from '@/components/home/HomeHeader';
+import UpComming from '@/components/home/UpComming';
+import CreateRoomButton from '@/components/roomlist/CreateRoomButton';
+import RoomList from '@/components/roomlist/RoomList';
+import { SUB_TITLE, TITLE } from '@/constants/title';
+import {
+  ContentContainer,
+  FlexColContainer,
+} from '@/styles/components/container';
+import { PageTitle } from '@/styles/components/text';
+import styled from 'styled-components';
+
+const mockRoom = [
+  {
+    title: '마이팀 방',
+    date: '2024. 07. 08 14:00~15:00',
+    member: '김민석님 외 12명',
+  },
+  {
+    title: '아자아자 방',
+    date: '2024. 07. 09 12:00~16:00',
+    member: '김민석님 외 12명',
+  },
+];
+
+function HomePage() {
+  return (
+    <FlexColContainer>
+      <ContentContainer>
+        <HomeHeader />
+        <PageTitle>{`윤아님! 반가워요\r\n일정을 효율적으로 관리해봐요`}</PageTitle>
+        <ContentWrapper>
+          <SubTitle>{SUB_TITLE.upcomming}</SubTitle>
+          <UpComming />
+        </ContentWrapper>
+        <ContentWrapper>
+          <SubTitle>{TITLE.attendRoom}</SubTitle>
+          <RoomList mock={mockRoom} />
+        </ContentWrapper>
+        <TotalButton>전체 모임보기</TotalButton>
+        <CreateRoomButton />
+      </ContentContainer>
+    </FlexColContainer>
+  );
+}
+
+const SubTitle = styled.h2`
+  color: ${({ theme }) => theme.color.secondary.solid.bk[700]};
+  ${({ theme }) => theme.typo.body.medium[20]}
+`;
+
+const TotalButton = styled.button`
+  background: none;
+  color: ${({ theme }) => theme.color.secondary.solid.bk[400]};
+  ${({ theme }) => theme.typo.body.semi_bold[12]}
+`;
+
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+`;
+export default HomePage;
