86c038bd190e3e6075fd5c1b4dad08afdcc5ecde
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-08-18 21:41:08 +0900

    [REFACTOR/HM-102]: RoomList 재상용 으로인한 변경

--- src/components/roomlist/RoomList.tsx
+++ src/components/roomlist/RoomList.tsx
@@ -1,50 +1,15 @@
 import styled from 'styled-components';
 import RoomItem from './RoomItem';
 
-const mock = [
-  {
-    title: '마이팀 방',
-    date: '2024. 07. 08 14:00~15:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '아자아자 방',
-    date: '2024. 07. 09 12:00~16:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '마이팀 방',
-    date: '2024. 07. 08 14:00~15:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '아자아자 방',
-    date: '2024. 07. 09 12:00~16:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '마이팀 방',
-    date: '2024. 07. 08 14:00~15:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '아자아자 방',
-    date: '2024. 07. 09 12:00~16:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '마이팀 방',
-    date: '2024. 07. 08 14:00~15:00',
-    member: '김민석님 외 12명',
-  },
-  {
-    title: '아자아자 방',
-    date: '2024. 07. 09 12:00~16:00',
-    member: '김민석님 외 12명',
-  },
-];
+interface Props {
+  mock: {
+    title: string;
+    date: string;
+    member: string;
+  }[];
+}
 
-function RoomList() {
+function RoomList({ mock }: Props) {
   return (
     <RoomListContainer>
       {mock.map((item) => (
