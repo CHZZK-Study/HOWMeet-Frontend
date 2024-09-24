79e3950a8febe691ad5f32b6995f472bfc304c28
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-25 08:47:43 +0900

    [Style/HM-154]: 일정 만들기 취소 모달 구현

---
+++ src/components/newmeeting/QuitMakeMeetingModal.tsx
@@ -0,0 +1,59 @@
+import Button from '@/components/common/Button';
+import { CloseIcon } from 'public/assets/icons';
+import styled, { useTheme } from 'styled-components';
+import { useQuitMakeMeetingModal } from '@/store/useModalStore';
+
+function QuitMakeMeetingModal() {
+  const theme = useTheme();
+  const closeQuit = useQuitMakeMeetingModal((state) => state.close);
+
+  return (
+    <QuitMakeMeetingModalContainer>
+      <CloseIcon
+        className="close-icon"
+        fill={theme.color.secondary.solid.bk[400]}
+        onClick={closeQuit}
+      />
+      <Title>일정 만들기를 중단하시겠어요?</Title>
+      <ButtonContainer>
+        <Button $style="solid">계속 진행</Button>
+        <Button $style="solid" $theme="primary-purple">
+          중단하기
+        </Button>
+      </ButtonContainer>
+    </QuitMakeMeetingModalContainer>
+  );
+}
+const QuitMakeMeetingModalContainer = styled.div`
+  position: relative;
+  width: 100%;
+  padding: 22px 24px 42px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 40px;
+  border-radius: 20px;
+  background: ${({ theme }) => theme.color.primary.white};
+
+  .close-icon {
+    position: absolute;
+    top: 22px;
+    right: 24px;
+    cursor: pointer;
+  }
+`;
+
+const Title = styled.h2`
+  margin-top: 76px;
+  color: ${({ theme }) => theme.color.secondary.solid.bk[900]};
+  ${({ theme }) => theme.typo.body.semi_bold[20]};
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 8px;
+`;
+
+export default QuitMakeMeetingModal;
