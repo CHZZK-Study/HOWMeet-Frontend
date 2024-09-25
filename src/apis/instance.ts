cdfbb08a6060e497e7f085efd1a72784d87324c9
JIMIN1020 <jimins4920@gmail.com>
2024-09-25 23:30:22 +0900

    [FEAT/HM-155]: 토큰 재발급 api 연동

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,9 +1,14 @@
 import { BASE_URL, DEFAULT_TIMOUT } from '@/constants/api';
+import { PATH } from '@/constants/path';
+import { STORAGE_KEY } from '@/constants/storage';
+import useUserStore from '@/store/userStore';
 import axios from 'axios';
+import { toast } from 'sonner';
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: DEFAULT_TIMOUT,
+  withCredentials: true,
 });
 
 axiosInstance.interceptors.request.use(
@@ -18,6 +23,38 @@ axiosInstance.interceptors.response.use(
     return res;
   },
   async (err) => {
-    return Promise.reject(err);
+    const { user, setUser } = useUserStore.getState();
+    const accessToken = localStorage.getItem(STORAGE_KEY.accessToken);
+
+    if (user && err.response.status === 401) {
+      const originRequest = err.config;
+      try {
+        const result = await axios.post(`${BASE_URL}/oauth/reissue`, {
+          headers: {
+            token: accessToken,
+          },
+        });
+
+        const newAccessToken = result.data.accessToken;
+
+        if (newAccessToken) {
+          localStorage.setItem(STORAGE_KEY.accessToken, newAccessToken);
+          originRequest.headers.token = newAccessToken;
+        }
+        return await axiosInstance(originRequest);
+      } catch (error) {
+        setUser(null);
+        localStorage.removeItem(STORAGE_KEY.accessToken);
+        toast.error('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+        window.location.href = PATH.login;
+        return Promise.reject(error);
+      }
+    } else {
+      setUser(null);
+      sessionStorage.removeItem(STORAGE_KEY.accessToken);
+      toast.error('로그인 세션이 만료되었습니다. 다시 로그인해주세요.');
+      window.location.href = PATH.login;
+      return Promise.reject(err);
+    }
   }
 );
