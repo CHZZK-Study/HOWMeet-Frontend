c52368ef2775a2fe0ff42167a8b65cd5fa768fb6
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-26 22:29:51 +0900

    [Feat/HM-144]: 방 리스트 응답 타입 수정

--- src/models/room.model.ts
+++ src/models/room.model.ts
@@ -1,17 +1,22 @@
 export interface RoomListRes {
-  roomId: string;
-  name: string;
-  memberSummary: string;
-  schedules: {
-    id: string;
-    dates: string[];
-    time: {
-      startTime: string;
-      endTime: string;
+  roomList: {
+    roomId: string;
+    name: string;
+    memberSummary: string;
+    schedules: {
+      id: string;
+      dates: string[];
+      time: {
+        startTime: string;
+        endTime: string;
+      };
+      name: {
+        value: string;
+      };
+      status: string;
     };
-    name: {
-      value: string;
-    };
-    status: string;
   }[];
+  currentPage: string;
+  hasNextPage: string;
+  totalPages: string;
 }
