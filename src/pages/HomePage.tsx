0c8984b5157b344f773b13e25b3060283eb6f106
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-26 22:31:34 +0900

    [Feat/HM-144]: 일정 없음 빈 컴포넌트 구현 및 참여중인 방 api 연결

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,26 +16,35 @@ import styled from 'styled-components';
 
 function HomePage() {
   const userInfo = sessionStorage.getItem('UserStore') || '';
-  const userId = JSON.parse(userInfo).state.user.id;
+  const parsedUserInfo = JSON.parse(userInfo);
+  const userId = parsedUserInfo.state.user.id;
+  const userName = parsedUserInfo.state.user.username;
+
   const { isOpen: isLogOutOpen, close: closeLogOut } = useLogOutModal();
-  const { roomList, isError } = useRoomList(userId);
+  const { roomListRes, isError } = useRoomList(userId);
 
   if (isError) window.alert('잠시후 다시 시도해 주세요.');
+  if (!roomListRes) return null;
 
-  if (!roomList) return null;
+  const roomList =
+    roomListRes.roomList.length === 0 ? [] : roomListRes.roomList;
 
   return (
     <FlexColContainer>
       <ContentContainer>
         <HomeHeader />
-        <PageTitle>{`윤아님! 반가워요\r\n일정을 효율적으로 관리해봐요`}</PageTitle>
+        <PageTitle>{`${userName}님! 반가워요\r\n일정을 효율적으로 관리해봐요`}</PageTitle>
         <ContentWrapper>
           <SubTitle>{SUB_TITLE.upcomming}</SubTitle>
           <UpComming />
         </ContentWrapper>
         <ContentWrapper>
           <SubTitle>{TITLE.attendRoom}</SubTitle>
-          <RoomList roomList={roomList} />
+          {roomList.length === 0 ? (
+            <EmptyBox>아직 참여중인 방이 없습니다</EmptyBox>
+          ) : (
+            <RoomList roomList={roomList} />
+          )}
         </ContentWrapper>
         <TotalButton>전체 모임보기</TotalButton>
         <CreateRoomButton />
@@ -64,5 +73,18 @@ const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 18px;
+`;
+
+const EmptyBox = styled.div`
+  width: 100%;
+  height: 270px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: ${({ theme }) => theme.color.primary.white};
+  border-radius: 14px;
+  box-shadow: 0px 3.805px 10.559px 0px rgba(90, 90, 90, 0.1);
+  color: ${({ theme }) => theme.color.secondary.solid.bk[500]};
+  ${({ theme }) => theme.typo.body.regular[20]}
 `;
 export default HomePage;
