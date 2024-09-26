c035a98af26eb7e46ccb1877957ed52db4935328
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-09-27 05:21:53 +0900

    [Feat/HM-144]: 비확정일정 컴포넌트 구현

--- src/components/roomdetail/NonConfirmList.tsx
+++ src/components/roomdetail/NonConfirmList.tsx
@@ -5,34 +5,26 @@ import {
   ListItem,
   NoticeDot,
 } from '@/styles/components/room/List';
+import { Schedule } from '@/types/room';
 import styled from 'styled-components';
 
-const mockNonConfirm = [
-  {
-    id: 1,
-    title: '전체 회의 일정',
-    date: '2024.07.08 - 2024.07.14',
-    notice: true,
-  },
-  {
-    id: 2,
-    title: '프론트엔드 2차 회의 일정',
-    date: '2024.07.11 - 2024.07.17',
-    notice: false,
-  },
-];
+interface Props {
+  progressMeetings: Schedule[];
+}
 
-function NonConfirmList() {
+function NonConfirmList({ progressMeetings }: Props) {
   return (
     <List>
-      {mockNonConfirm.map((item) => (
+      {progressMeetings.map((item) => (
         <NonConfirmListItem key={item.id}>
-          <h1 className="title">{item.title}</h1>
+          <h1 className="title">{item.name.value}</h1>
           <DateWrapper>
             <Badge>예정된 일정 기간</Badge>
-            <p className="date">{item.date}</p>
+            <p className="date">
+              {item.dates[0]} ~ {item.dates[1]}
+            </p>
           </DateWrapper>
-          {item.notice && <NoticeDot />}
+          <NoticeDot />
         </NonConfirmListItem>
       ))}
     </List>
