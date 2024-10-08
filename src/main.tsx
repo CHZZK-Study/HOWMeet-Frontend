cd48f897f66492bc66972a7a8bd88f7fb2cd2ae8
oyeong011 <oyeong011@gmail.com>
2024-10-08 16:47:52 +0900

    [FIX/HM-145] 모킹서버 종료

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const mocking = async (): Promise<void> => {
-  const isDev = import.meta.env.DEV;
-  if (!isDev) {
-    const { worker } = await import('./mocks/browser');
-    worker.start();
-  }
-};
+// const mocking = async (): Promise<void> => {
+//   const isDev = import.meta.env.DEV;
+//   if (!isDev) {
+//     const { worker } = await import('./mocks/browser');
+//     worker.start();
+//   }
+// };
 
-mocking().then(() => {
-  ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
-});
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
